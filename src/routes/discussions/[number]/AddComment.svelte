4192d2a94d50dcd6c20b7e5659cfa93defbf619d
Zach Arend <zachzach@google.com>
2023-07-20 21:03:52 +0000

    style AddComment using tailwind

--- src/routes/discussions/[number]/AddComment.svelte
+++ src/routes/discussions/[number]/AddComment.svelte
@@ -1,4 +1,4 @@
-<form method="POST" action="?/comment">
-  <textarea name="contents"></textarea>
-  <button type="submit">Post Comment</button>
+<form method="POST" action="?/comment" class="m-4 p-2">
+  <textarea name="contents" class="textarea p-2"></textarea>
+  <button type="submit" class="btn variant-filled">Post</button>
 </form>
